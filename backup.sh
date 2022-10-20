71d4a6de52ef8ebdeaba97de7b4ad7285beda9de
Kyrylo <ovsyukkm@gmail.com>
2022-10-21 00:26:21 +0200

    Improved readability. Repaired bugs
    Improved readability of script file output. Repaired bug where files were not deleted on target systems

--- backup.sh
+++ backup.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
-rsync -Pav /home/ll1rro/Education /home/ll1rro/Backup
-rsync -Pav /home/ll1rro/Documents /home/ll1rro/Backup
-rsync -Pav --exclude 'Screenshots' /home/ll1rro/Pictures /home/ll1rro/Backup
-rsync -Pav /home/ll1rro/Work /home/ll1rro/Backup
+#rsync -Pav /home/ll1rro/Education /home/ll1rro/Backup
+#rsync -Pav /home/ll1rro/Documents /home/ll1rro/Backup
+#rsync -Pav --exclude 'Screenshots' /home/ll1rro/Pictures /home/ll1rro/Backup
+#rsync -Pav /home/ll1rro/Work /home/ll1rro/Backup
 
-aws s3 sync /home/ll1rro/Backup s3://ll1rro-forbackups/Backup
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+echo
+echo -e "${GREEN}Sending all files from directories 'Education, Documents, Pictures and Work' to Backup directory${NC}"
+echo
+rsync -Pav --exclude 'Screenshots' --delete /home/ll1rro/Education /home/ll1rro/Documents /home/ll1rro/Pictures /home/ll1rro/Work /home/ll1rro/Backup
+
+echo
+echo
+echo -e "${GREEN}Starting sync between your system and AWS S3 backet${NC}"
+echo
+/usr/local/bin/aws s3 sync --delete /home/ll1rro/Backup s3://ll1rro-forbackups/Backup
